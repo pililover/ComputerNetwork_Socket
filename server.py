acc2499a1ff4f04eb5845c12a3b8f9f8934e97c6
pililover <nkanh22@clc.fitus.edu.vn>
2023-08-01 19:18:28 +0700

    Update server.py

--- server.py
+++ server.py
@@ -68,3 +68,54 @@ def doClient(Cli_Sock, Cli_Addr):
     finally:
         Cli_Sock.close()
         
+class Account:
+    def __init__(self, user, password):
+        self.user = user
+        self.password = password
+    
+    def checkAvailable(self):
+        with open("account.csv", "r") as fin:
+            for line in fin:
+                user, password = line.strip().split(',')
+                if user == self.user and password == self.password:
+                    return True
+        return False
+
+    def addNewAcc2File(self):
+        with open("account.csv", "a") as fin:
+            fin.write(self.user + "," + self.password + "\n")
+
+    def createAccount(self):
+        if not self.checkAvailable():
+            self.addNewAcc2File()
+            print("Create account success")
+            return True
+        else:
+            print("Create account fail")
+            return False
+    
+
+def register(Cli_Sock, Cli_Addr):
+    user = Cli_Sock.recv(1024).decode("utf8")
+    print("Client register: " + s)
+    print("Username: ", user)
+    Cli_Sock.sendall(user.encode('utf8'))
+    
+    password = Cli_Sock.recv(1024).decode("utf8")
+    print("Password: ", password)
+    Cli_Sock.sendall(password.encode('utf8'))
+    
+    acc = Account(user, password)
+    valid_acc = acc.checkAvailable()
+    
+    if valid_acc == True:
+        Cli_Sock.sendall(bytes("Account is available", "utf8"))
+    else:
+        Cli_Sock.sendall(bytes("Account is not available", "utf8"))
+        acc.createAccount()
+    
+    print ("Register process is done")
+    
+def login(Cli_Sock, Cli_Addr):
+    
+    
