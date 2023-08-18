93d136815190d4bfef31a5e76e15783abf2afb6c
pililover <nkanh22@clc.fitus.edu.vn>
2023-08-18 21:50:47 +0700

    Update client.py

--- Client/client.py
+++ Client/client.py
@@ -88,6 +88,7 @@ class Client:
         self.root = tk.Tk()
         if res == "APPLICATION":
             data = self.receive_data()
+            self.gui.appScene
             # gui.GUI.appScene
             # Display data in a tkinter widget
             tk.Label(self.root, text=data).pack()
@@ -102,6 +103,7 @@ class Client:
         elif res == "TAKEPIC":
             # Receive image data from the server
             data = self.receive_data()
+            self.gui.ScreenshotScene
             # gui.GUI.ScreenshotScene
             size = int(self.Cli_Sock.recv(10).decode('utf-8'))
             print(size)
@@ -109,18 +111,20 @@ class Client:
             the_photo = self.Cli_Sock.recv(size)
 
             print(the_photo)
-            img_to_save = Image.frombytes("RGB", (490, 490), the_photo)
-            img_to_save.save("screenshot.png")
+            img_to_save = Image.frombytes("RGB", (500, 500), the_photo)
+            img_to_save.save("screen.png")
 
             # Display data in a tkinter widget
             tk.Label(self.root, text=data).pack()            
         elif res == "KEYLOG":
             data = self.receive_data()
+            self.gui.keystrokeScene
             # gui.GUI.keystrokeScene
             # Display data in a tkinter widget
             tk.Label(self.root, text=data).pack()
         elif res == "PROCESS":
             data = self.receive_data()
+            self.gui.processScene
             # gui.GUI.processScene
             # Display data in a tkinter widget
             tk.Label(self.root, text=data).pack()
